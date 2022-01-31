8e09830092957b12d101c85e236d4c9db4ba785b
karl-maker <karljohanbailey98@gmail.com>
2022-01-30 21:25:21 -0400

    _error changes

--- pages/_error.js
+++ pages/_error.js
@@ -4,20 +4,10 @@ import { MdOutlineError } from "react-icons/md";
 
 function NotFound() {
   return (
-    <div style={{ height: "100%" }} className="container mt-5">
-      <div className="text-center align-middle my-auto">
-        <MdOutlineError
-          className="display-1 m-3"
-          tabindex="0"
-          data-toggle="tooltip"
-          title="404 Error"
-        />
-        <h1 className="display-2">Looks Like You Are Lost.</h1>
-        <p>Let's deliver you back to the home page</p>
-        <Link href="/" passHref>
-          Go Back Home
-        </Link>
-      </div>
+    <div className="text-center align-middle my-auto mt-5">
+      <MdOutlineError className="display-1" />
+      <h1 className="display-2 m-3">Looks Like You Are Lost.</h1>
+      <Link href={"/"}>Let us deliver you back to the home page</Link>
     </div>
   );
 }
