f4e15a1f027ef3966f4105b81e236ee6dcd89b14
karl-maker <karljohanbailey98@gmail.com>
2022-02-02 00:51:49 -0400

    Breadcrumbs
    Using MUI Code

--- pages/help.js
+++ pages/help.js
@@ -1,31 +1,44 @@
+import { Breadcrumbs, Typography } from "@mui/material";
 import Link from "next/link";
 
 function Help() {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12 m-0  container-fluid">
-          {/*
+    <>
+      <div className="col-12 p-3">
+        <div role="presentation" className="col-12">
+          <Breadcrumbs aria-label="breadcrumb" className="breadcrumbs">
+            <Typography>
+              <Link href="/" passHref>
+                Home
+              </Link>
+            </Typography>
+            <Typography>Help</Typography>
+          </Breadcrumbs>
+        </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12 m-0  container-fluid">
+              {/*
 
                 RightHandSide
 
                 */}
-
-          <div className="col-12 m-0 ">{/* Breadcrumb  */}</div>
-          <div className="col-12 m-0 "></div>
-        </div>
-        <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12  m-0 container">
-          {/*
+            </div>
+            <div className="col-12 m-0 "></div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12  m-0 container">
+            {/*
 
                 LeftHandSide
 
                 */}
-          <div className="row">
-            <div className="col-12 m-0 "></div>
+            <div className="row">
+              <div className="col-12 m-0 "></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import { useContext, useState, useEffect } from "react";
+import { Breadcrumbs, Typography } from "@mui/material";
 import { DeviceContext } from "../context/wrapper";
 import WidgetWrapper from "../components/widgets/wrapper";
 
 function Home() {
   var deviceData = useContext(DeviceContext);
 
-  return <div></div>;
+  return (
+    <div className="col-12 p-3">
+      <div role="presentation" className="col-12">
+        <Breadcrumbs aria-label="breadcrumb" className="breadcrumbs">
+          <Typography>Home</Typography>
+        </Breadcrumbs>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
