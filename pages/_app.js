414ba83e8f81393a377426a07536e684286b4384
karl-maker <karljohanbailey98@gmail.com>
2022-02-02 23:21:26 -0400

    No Zoom

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
